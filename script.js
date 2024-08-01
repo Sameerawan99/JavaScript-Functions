27304ba1bdc4009d1e4879b3e2d1beb16e3c7a98
Sameerawan99 <165399018+Sameerawan99@users.noreply.github.com>
2024-08-01 21:41:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+//   TASK #1
+// function addnumber(num1,num2){
+//     return num1+num2;
+// }
+
+// let a = addnumber(5,1);
+// console.log(a)
+
+
+
+//   TASK #2
+
+// function uppercase(str){
+//     return     str.toUpperCase()
+// }
+// let b = uppercase("hello");
+// console.log(b);
+
+// let c = uppercase("world");
+// console.log(c);
+
+
+// TASK # 3 AVERAGE OF ARRAY
+
+
+// function average(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//       sum += arr[i];
+//     }
+//     return sum / arr.length;
+//   }
+  
+//   let d = average([1, 2, 3, 4, 5]);
+//   console.log(d);
+
+
+
+//   TASK #4
+
+// function removevowels(str){
+
+//     let vowels = "aeiouAEIOU";
+//     let result = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if (!vowels.includes(str[i])) {
+//             result += str[i];
+//             }
+        
+//     }
+//     return result;
+// }
+
+// let a = removevowels("javascript")
+// console.log(a);
+
+// let c = removevowels("hello world");
+// console.log(c);
+
+
+
+
+// TASK #5
+
+// function factorial(num){
+    
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+//         result = result* i;
+// }
+// return result;
+// }
+// let b = factorial(5);
+// console.log(b);
+
+
+// TASK #06
+
+// function reversestring(str){
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+
+// }
+
+// return reversed;
+// }
+//  let a = reversestring("hello")
+       
+//  console.log(a);
+  
+//  let b = reversestring("world");
+//  console.log(b);
+
+
+
